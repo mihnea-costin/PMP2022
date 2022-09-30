1f6a17083268c63e86cad267fa14d7fd961cf582
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-09-30 11:30:23 +0300

    Lab1

--- lab1.py
+++ lab1.py
@@ -19,13 +19,29 @@ X2 = np.random.randn(size) * 0.2
 Y = alpha + beta[0] * X1 + beta[1] * X2 + np.random.randn(size) * sigma
 
 # Vizualize the data
-fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10, 4))
+fig, axes = plt.subplots(1, 3, sharex=True, figsize=(10, 4))
 axes[0].scatter(X1, Y, alpha=0.6)
 axes[1].scatter(X2, Y, alpha=0.6)
 axes[0].set_ylabel("Y")
 axes[0].set_xlabel("X1")
 axes[1].set_xlabel("X2")
+
+mu, sigma = 1, 1 # mean and standard deviation
+s = np.random.normal(mu, sigma, 1000)
+
+abs(mu - np.mean(s))
+0.0  # may vary
+
+abs(sigma - np.std(s, ddof=1))
+0.1  # may vary
+
+count, bins, ignored = plt.hist(s, 30, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+         linewidth=2, color='r')
+
 plt.show()
+
 
 # Define a model
 basic_model = pm.Model()
@@ -46,13 +62,3 @@ with basic_model:
 #Fit model to data and print estimates
 map_estimate = pm.find_MAP(model=basic_model)
 print(map_estimate)
-
-x = random.normal(size=(2, 3))
-
-print(x)
-
-count, bins, ignored = plt.hist(s, 30, density=True)
-plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
-               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
-         linewidth=2, color='r')
-plt.show()
