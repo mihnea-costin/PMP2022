dee64a36269e2c2f00e4dd6760d1819ddc3092ae
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-10-12 22:39:30 +0300

    Lab2

--- Lab2PMP.py
+++ Lab2PMP.py
@@ -1,5 +1,39 @@
 #ex.1
 
+# import numpy as np
+# from scipy import stats
+
+# import statistics
+# import matplotlib.pyplot as plt
+# import arviz as az
+
+# np.random.seed(1)
+
+# x = stats.binom.rvs(1, 0.6, size=10000) # Distributie uniforma intre -1 si 1, 1000 samples . Primul parametru fiind limita inferioara a intervalului, al doilea parametru fiind "marimea" intervalului, aka [-1,-1+2] = [-1,1]
+# a = []
+
+# for i in range(0, 10000):
+#     choose = stats.binom.rvs(n=1, p=0.4, size=1)
+#     if choose == 1:
+#         # x.append(stats.expon.rvs(0, scale= 1/4, size=1)[0])
+#         x = np.append(x,stats.expon.rvs(0, scale= 1/4, size=1)[0])
+#     else:
+#         x = np.append(x,stats.expon.rvs(0, scale= 1/6, size=1)[0])
+
+# medie = np.mean(x) 
+# std = np.std(medie)
+
+# print(std)
+# az.plot_posterior({'x':x}) # Afisarea aproximarii densitatii probabilitatilor, mediei, intervalului etc. variabilelor x,y,z
+# plt.show() 
+
+#ex.2
+
+# distributia gama(alpha,lambda) se poate apela cu stats.gamma(alpha,0,1/lambda)
+# sau stats.gamma(alpha, scale=1/lambda)
+
+#ex3
+
 import numpy as np
 from scipy import stats
 
@@ -7,25 +41,23 @@ import statistics
 import matplotlib.pyplot as plt
 import arviz as az
 
-np.random.seed(1)
+np.random.seed(10)
 
-lambda1=4
-lambda2=6
+x = stats.binom.rvs(1, 0.6, size=10000)
+a = []
 
-x = stats.uniform.rvs(-1, 2, size=10000) # Distributie uniforma intre -1 si 1, 1000 samples . Primul parametru fiind limita inferioara a intervalului, al doilea parametru fiind "marimea" intervalului, aka [-1,-1+2] = [-1,1] 
-#y=(lambda1+1,4*lambda2)/2 # Compunerea prin insumare a celor 2 distributii
+for i in range(0, 100):
+    choose = stats.binom.rvs(n=1, p=0.3, size=2)
+    if choose == 1:
+        # x.append(stats.expon.r vs(0, scale= 1/4, size=1)[0])
+        x = np.append(x,stats.expon.rvs(0, scale= 1/5, size=1)[0])
+    else:
+        x = np.append(x,stats.expon.rvs(0, scale= 1/3, size=1)[0])
 
-m1=stats.expon(0,1/lambda1)
-m2=stats.expon(0,1/lambda2)
+medie = np.mean(x) 
+std = np.std(medie)
 
-x=1,5*lambda1+lambda2
-
-print("Deviatia standard a lui x este % s "% (statistics.stdev(x)))
-print("Media lui x este % s " % (statistics.mean(x))) 
-
-#y=(m1*1,4*m2)/2
-
-az.plot_posterior({'x':x,'m1':m1,'m2':m2}) # Afisarea aproximarii densitatii probabilitatilor, mediei, intervalului etc. variabilelor x,y,z
+print(std)
+az.plot_posterior({'x':x}) # Afisarea aproximarii densitatii probabilitatilor, mediei, intervalului etc. variabilelor x,y,z
 plt.show() 
-
 
