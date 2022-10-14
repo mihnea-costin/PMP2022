949ecf03c8148a1b2fa697c63019d2f638d5438a
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-10-14 11:27:54 +0300

    Laboratorul 3 modificari

--- Laboratorul3.py
+++ Laboratorul3.py
@@ -6,12 +6,35 @@ import arviz as az
 
 model = pm.Model()
 
+#1) modelul probabilist
 with model:
-    cutremur = pm.Bernoulli('C', 0.005)
-    incendiu = pm.Bernoulli('I', 0.01)
-    alarmaincendiu_p = pm.Deterministic('AI_p', pm.math.switch(incendiu, pm.math.switch(cutremur, 0.01, 0.03), pm.math.switch(cutremur, 0.8, 0.2)))
-    accidental_p = pm.Deterministic('C_p', pm.math.switch(reducere, pm.math.switch(urgent, 1, 0.5), pm.math.switch(urgent, 0.8, 0.2)))
-    neaccidental_p = pm.Deterministic('C_p', pm.math.switch(reducere, pm.math.switch(urgent, 1, 0.5), pm.math.switch(urgent, 0.8, 0.2)))
+    cutremur = pm.Bernoulli('C', 0.0005)
 
-    cumpara = pm.Bernoulli('C', p=cumpara_p)
+    incendiu_p = pm.Deterministic('I_p', pm.math.switch(cutremur, 0.01, 0.03))
+    incendiu = pm.Bernoulli('I', incendiu_p)
+
+    alarma_p = pm.Deterministic('A_p', pm.math.switch(incendiu, pm.math.switch(cutremur, 0.01, 0.02), pm.math.switch(cutremur, 0.95, 0.98)))
+    alarma = pm.Bernoulli('C', p=alarma_p, observed=1)
+
     trace = pm.sample(20000)
+
+dictionary = {
+          'cutremur': trace['C'].tolist(),
+          'incendiu': trace['I'].tolist(),
+          'alarma': trace['A'].tolist()
+          }
+
+df = pd.DataFrame(dictionary)    
+
+#2) prob cutremur 
+
+p_cutremur = df[(df['cutremur'] == 1)].shape[0] / df.shape[0]
+print(p_cutremur)
+
+#3) prob incendiu
+
+p_incendiu = df[(df['incendiu'] == 1)].shape[0] / df.shape[0]
+print(p_incendiu)
+
+#4) calculati cele 2 probabilitati folosind regula lui Bayes
+
