6513ac37ddeac142cf94aafbc53a050d5b0e4911
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-10-26 17:41:14 +0300

    Lab 4 PMP
    Am facut punctele 2 si 3

--- Laboratorul4.py
+++ Laboratorul4.py
@@ -11,8 +11,14 @@ nr_clienti = stats.Poisson.rvs(0,20, size=10000) # Distributie Poisson a numarul
 timp_plasare_plata = stats.normal.rvs(0,1,1/2)
 pregatire = stats.expon.rvs(0, 1/20 , size=10000)
 
-# 2)
+# 2) Determinaţi care este (cu aproximaţie) α maxim pentru a le putea servi mancarea intr-un timp mai
+# scurt de 15 minute tuturor clienţilor care intră într-o oră, cu o probabilitate de 95%. 
 
-# 3)
+alpha_maxim_pentru_servire_rapida = stats.norm.ppf(0.95, 0, 1/2)
 
-# 4)
+# 3) Cu α astfel calculat, determinaţi timpul mediu de aşteptare pentru a fi servit al unui client.
+
+timpul_mediu_de_asteptare= stats.norm.interval(alpha_maxim_pentru_servire_rapida, 0, 1/2)
+
+# 4) Bonus: Generaţi un eşantion de 100 de timpi de aşteptare medii dintre care 95% sunt sub 15 minute şi
+# cu aceste date inferaţi asupra lui α. Este acest α comparabil cu cel găsit mai sus?
