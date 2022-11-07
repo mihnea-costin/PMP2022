629aac9ad7b588c6356abe3f5da32c8b6ce528f8
mihnea-costin <79205548+mihnea-costin@users.noreply.github.com>
2022-11-07 23:01:32 +0200

    Laboratorul 6
    Am rezolvat problema 4

--- Laboratorul6.py
+++ Laboratorul6.py
@@ -25,7 +25,7 @@ plt.xticks(())
 plt.yticks(())
 
 #Afisare grafic
-# plt.show()
+plt.show()
 
 # 2. Definiţi modelul Bayesian de regresie liniară (folosind PyMC3) care sa descrie contextul de mai sus. (2pt -
 # deadline: sfârşitul seminarului)
@@ -46,7 +46,7 @@ with pm.Model() as model_g:
 # 3. Determinaţi care este dreapta de regresie care se potriveşte cel mai bine datelor. La ce vârstă aţi recomanda
 # mamelor să nască? (2pt - deadline: sfârşitul seminarului)
 
-posterior_g = idata_g.posterior.stack(samples={"momage", "ppvt"})
+posterior_g = idata_g.posterior.stack(samples={"educ_cat", "ppvt"})
 
 # Afisare dreapta de regresie
 print(posterior_g)
@@ -59,6 +59,38 @@ print(posterior_g)
 # 4. Repetaţi acum cerinţele de mai sus schimbând “vârsta” cu “nivelul de educaţie al” mamei. S-au schimbat
 # concluziile relativ la momentul naşterii? (3pt - deadline: luni, 7.11.2022)
 
+Y = df['educ_cat']
+X = df['ppvt']
+
+X=X.values.reshape(len(X),1)
+Y=Y.values.reshape(len(Y),1)
+
+# Plot 
+plt.scatter(X, Y,  color='black')
+plt.title('Test Data')
+plt.xlabel('Education Level')
+plt.ylabel('IQ')
+plt.xticks(())
+plt.yticks(())
+
+#Afisare grafic
+plt.show()
+
+#Modelul Bayesian de regresie liniara
+
+with pm.Model() as model_g:
+    α = pm.Normal('α', mu=0, sd=50)
+    β = pm.Normal('β', mu=0, sd=10)
+    ε = pm.HalfCauchy('ε', 5)
+    μ = pm.Deterministic('μ', α + β * X)
+    y_pred = pm.Normal('y_pred', mu=μ, sd=ε, observed=Y)
+    
+    idata_g = pm.sample(2000, tune=2000, return_inferencedata=True)
+
+# Afisare dreapta de regresie
+print(posterior_g)
+
 # Bonus: Reprezentaţi grafic un set de 100 de date generate conform distribuţiei predictive a posteriori, îm-
 # preună cu regiunea 97%HPD pentru acestea (1pt pentru fiecare caz, cu deadline-urile respective: sfârşitul
 # seminarului şi luni, 7.11.2022).
+
